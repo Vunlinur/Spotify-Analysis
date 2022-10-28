9529b9540ebc28490dde52bb17ee8984f13caf08
Linur <Adam.Porecki@lionbridge.com>
2022-10-28 22:10:46 +0200

    Extract PieChart component

---
+++ SpotifyAnalysis/Data/Element.cs
@@ -0,0 +1,15 @@
+﻿using SpotifyAPI.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpotifyAnalysis.Data {
+	public class Elements : List<Element> { }
+
+	public class Element {
+		public string Label { get; set; }
+		public int Quantity { get; set; }
+		public string Color { get; set; }
+	}
+}
