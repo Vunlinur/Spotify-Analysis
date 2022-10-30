8458d119f085468c665a954dd803b63817e7205b
Linur <Adam.Porecki@lionbridge.com>
2022-10-30 03:09:56 +0100

    Create pie chart prototype

--- SpotifyAnalysis/Data/Element.cs
+++ SpotifyAnalysis/Data/Element.cs
@@ -1,11 +1,23 @@
 ﻿using SpotifyAPI.Web;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SpotifyAnalysis.Data {
-	public class Elements : List<Element> { }
+	public class Elements : KeyedCollection<string, Element> {
+		protected override string GetKeyForItem(Element item) {
+			return item.Label;
+		}
+
+		public void Increase(Element item) {
+			if (Contains(item.Label))
+				this[item.Label].Quantity += item.Quantity;
+			else
+				Add(item);
+		}
+	}
 
 	public class Element {
 		public string Label { get; set; }
