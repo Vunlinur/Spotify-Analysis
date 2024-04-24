a1a61ad74636616ac96028f0a3915756050b00bd
Vunlinur <mightymetalfan@gmail.com>
2024-04-24 20:51:36 +0200

    Fix logic for empty db initialization; Add more converter extensions

--- SpotifyAnalysis/Data/SpotifyAPI/SpotifyToDTOExtensions.cs
+++ SpotifyAnalysis/Data/SpotifyAPI/SpotifyToDTOExtensions.cs
@@ -1,49 +1,68 @@
 ﻿using SpotifyAnalysis.Data.DTO;
 using SpotifyAPI.Web;
+using System;
 using System.Collections.Generic;
 
 namespace SpotifyAnalysis.Data.SpotifyAPI {
     public static class SpotifyToDTOExtensions {
-        // TODO Add recursive field conversion
 		public static UserDTO ToUserDTO(this PublicUser pu) {
             return new UserDTO() {
                 ID = pu.Id,
-                Name = pu.DisplayName
+                Name = pu.DisplayName,
+				Playlists = []
 			};
 		}
 
-        public static PlaylistDTO ToPlaylistDTO(this FullPlaylist fp) {
+		public static PlaylistDTO ToPlaylistDTO(this FullPlaylist fp) {
             return new PlaylistDTO() {
                 ID = fp.Id,
                 Name = fp.Name,
                 Owner = fp.Owner.Id,
                 SnapshotID = fp.SnapshotId,
-                Followers = fp.Followers?.Total ?? -1,
                 Tracks = [],
                 Images = []
-            };
+			};
 		}
 
-        public static TrackDTO ToTrackDTO(this FullTrack ft) {
-            return new TrackDTO() {
-                ID = ft.Id,
-                Name = ft.Name,
-                DurationMs = ft.DurationMs,
-                Popularity = ft.Popularity,
-                Album = null,
-                Artists = []
-            };
-        }
+		public static TrackDTO ToTrackDTO(this FullTrack ft) {
+			return new TrackDTO() {
+				ID = ft.Id,
+				Name = ft.Name,
+				DurationMs = ft.DurationMs,
+				Popularity = ft.Popularity,
+				Album = null,
+				Artists = []
+			};
+		}
 
-        public static ArtistDTO ToArtistDTO(this FullArtist fa) {
-            return new ArtistDTO() {
-                ID = fa.Id,
-                Name = fa.Name,
-                Genres = fa.Genres,
-                Popularity = fa.Popularity,
-                Albums = [],
-                Images = []
-            };
-        }
-    }
+		public static AlbumDTO ToAlbumDTO(this FullAlbum a) {
+			return new AlbumDTO() {
+				ID = a.Id,
+				Name = a.Name,
+				ReleaseDate = a.ReleaseDate,
+				TotalTracks = a.TotalTracks,
+				Artists = [], // SimpleArtist instead of FullArtist
+				Tracks = [],
+				Images = []
+			};
+		}
+
+		public static ArtistDTO ToArtistDTO(this FullArtist fa) {
+			return new ArtistDTO() {
+				ID = fa.Id,
+				Name = fa.Name,
+				Genres = fa.Genres,
+				Popularity = fa.Popularity,
+				Albums = [],
+				Images = []
+			};
+		}
+
+		public static ImageDTO ToImageDTO(this Image i) {
+			return new ImageDTO() {
+                Url = i.Url,
+                Resolution = Math.Min(i.Height, i.Width)
+			};
+		}
+	}
 }
