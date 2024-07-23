4543021f643b7a13340c9f11315b8d0e4b4c381c
Vunlinur <mightymetalfan@gmail.com>
2024-07-23 04:39:09 +0200

    Add StaticReourceMap

---
+++ SpotifyAnalysis/Shared/StaticResourceMap.cs
@@ -0,0 +1,11 @@
+﻿namespace SpotifyAnalysis.Shared
+{
+    public static class StaticResourceMap
+    {
+        public const string UnknownUser = "anonymous-user.jpeg";
+        public const string UnknownResource = "unknown.jpeg";
+        public const string UnknownArtist = UnknownResource;
+        public const string UnknownPlaylist = UnknownResource;
+        public const string UnknownAlbum = UnknownResource;
+    }
+}
