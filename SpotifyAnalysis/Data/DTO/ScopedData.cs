bd6713279d36b9c583ef38b0eb9f250fba4c069e
Vunlinur <mightymetalfan@gmail.com>
2024-08-03 02:29:00 +0200

    Add selected user avatar to appBar; Lock pages behind scoped data; Add refresh event when changing scoped UserDTO

--- SpotifyAnalysis/Data/DTO/ScopedData.cs
+++ SpotifyAnalysis/Data/DTO/ScopedData.cs
@@ -1,5 +1,13 @@
-﻿namespace SpotifyAnalysis.Data.DTO {
+﻿using System;
+
+namespace SpotifyAnalysis.Data.DTO {
 	public class ScopedData {
-		public UserDTO UserDTO;
-	}
+        public event Action<UserDTO> UserChanged;
+
+        public UserDTO UserDTO {
+			get => _user;
+			set => UserChanged?.Invoke(_user = value);
+        }
+        private UserDTO _user;
+    }
 }
