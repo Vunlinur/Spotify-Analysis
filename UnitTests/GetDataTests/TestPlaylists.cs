72d9ba3bbce3b7cac7d94c8210570ec24589361b
Vunlinur <mightymetalfan@gmail.com>
2025-01-09 04:03:20 +0100

    Add Remove_PlaylistTree test

--- UnitTests/GetDataTests/TestPlaylists.cs
+++ UnitTests/GetDataTests/TestPlaylists.cs
@@ -44,6 +44,34 @@ namespace Tests.GetDataTests {
         }
 
         [Test]
+        public async Task Remove_PlaylistTree() {
+            // Arrange
+            var testArtist = Stubs.FullArtist();
+            var testSimpleArtist = Stubs.SimpleArtist();
+            var testAlbum = Stubs.SimpleAlbum([testSimpleArtist]);
+            var testTrack = Stubs.FullTrack([testSimpleArtist], testAlbum);
+            var testPlaylist = Stubs.FullPlaylist(testUser, [testTrack]);
+
+            mockPublicPlaylists.SetupSequence(m => m(It.Is<string>(s => s == testUser.Id)))
+                .Returns(Task.FromResult<IList<FullPlaylist>>([testPlaylist]))
+                .Returns(Task.FromResult<IList<FullPlaylist>>([]));
+            mockPlaylist.Setup(m => m(It.Is<string>(s => s == testPlaylist.Id)))
+                .Returns(Task.FromResult(testPlaylist));
+            mockTracks.Setup(m => m(It.IsAny<Paging<PlaylistTrack<IPlayableItem>>>()))
+                .Returns(Task.FromResult(new List<FullTrack> { testTrack }));
+            mockArtists.Setup(m => m(It.IsAny<IList<string>>()))
+                .Returns(Task.FromResult(new List<FullArtist> { testArtist }));
+
+            // Act
+            var dataFetch = CreateDataFetch();
+            await dataFetch.GetData(testUser.Id);
+            await dataFetch.GetData(testUser.Id);
+
+            // Assert
+            AssertDbSetCounts(0, 1, 1, 1);
+        }
+
+        [Test]
         public async Task Rename_Playlist() {
             // Arrange
             var testPlaylist = Stubs.FullPlaylist(testUser, []);
