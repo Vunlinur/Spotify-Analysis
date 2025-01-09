e7bdc5b2d33bb49f847d026a8436d8078feb862b
Vunlinur <mightymetalfan@gmail.com>
2025-01-09 03:49:03 +0100

    Refresh access token in SpotifyClientStatic

--- SpotifyAnalysis/Data/SpotifyAPI/SpotifyClientStatic.cs
+++ SpotifyAnalysis/Data/SpotifyAPI/SpotifyClientStatic.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.IdentityModel.Tokens;
 using SpotifyAPI.Web;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 
 namespace SpotifyAnalysis.Data.SpotifyAPI {
@@ -16,27 +18,38 @@ namespace SpotifyAnalysis.Data.SpotifyAPI {
     public class SpotifyClientStatic {
 		public SpotifyClient SpotifyClient { get; set; }
 
+        private Timer refreshTimer;
+
         public SpotifyClientStatic() {
-            InitializeSpotifyClient();
+            Task.Run(InitializeSpotifyClient);
         }
 
         private void InitializeSpotifyClient() {
 			var config = SpotifyClientConfig.CreateDefault();
-
 			var credentials = new ClientCredentialsRequest(
 				Program.Config.GetValue<string>("ClientId"),
 				Program.Config.GetValue<string>("ClientSecret")
 			);
 
-			try {
-				// TODO handle server errors like Error SQL80001: An expression of non-boolean type specified in a context where a condition is expected.
-				var response = new OAuthClient(config).RequestToken(credentials);
-				response.Wait(); // Async await did not return with an error but timed out instead: TODO test when API's down
-				SpotifyClient = new SpotifyClient(config.WithToken(response.Result.AccessToken));
-			}
-			catch (SecurityTokenExpiredException) {
-				throw; // TODO refresh token
-			}
-		}
+            int refreshTime;
+            try {
+                // TODO handle server errors like Error SQL80001: An expression of non-boolean type specified in a context where a condition is expected.
+                var response = new OAuthClient(config).RequestToken(credentials);
+                response.Wait(); // Async await did not return with an error but timed out instead: TODO test when API's down
+                SpotifyClient = new SpotifyClient(config.WithToken(response.Result.AccessToken));
+
+                refreshTime = response.Result.ExpiresIn - 30; // Refresh 30 seconds before expiry
+            }
+            catch (Exception e) {
+                refreshTime = 30;  // retry in 30 sec
+                // TODO logging
+            }
+
+            // TODO no need to refresh the token when noone is connected to the server:
+            // https://stackoverflow.com/questions/19313339/refreshing-access-token-only-when-necessary
+            // https://www.reddit.com/r/Blazor/comments/10s5t7p/blazor_server_how_to_count_active_connections/
+            refreshTimer?.Dispose();
+            refreshTimer = new Timer(_ => InitializeSpotifyClient(), null, TimeSpan.FromSeconds(refreshTime), Timeout.InfiniteTimeSpan);
+        }
     }
 }
