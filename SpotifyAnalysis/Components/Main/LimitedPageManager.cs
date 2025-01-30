88a4f102cd23aa05d8ba5cac1fc700b25106a4c5
Vunlinur <mightymetalfan@gmail.com>
2025-01-30 19:35:30 +0100

    collect pages that use injected ScopedData to block them inclusively instead of exclusively

---
+++ SpotifyAnalysis/Components/Main/LimitedPageManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using SpotifyAnalysis.Data.Common;
+using System;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace SpotifyAnalysis.Components {
+    public static class LimitedPageManager {
+        private const string pageNamespace = "SpotifyAnalysis.Pages";
+        private static readonly string[] limitedPages;
+
+        public static bool IsCurrentPageLimited(NavigationManager navigation) => 
+            limitedPages.Contains(navigation.ToBaseRelativePath(navigation.Uri));
+
+        static LimitedPageManager() {
+            // 1. Get all page types - must have RouteAttribute
+            var pages = Assembly.GetExecutingAssembly().GetTypes().Where(t => 
+                t.IsClass &&
+                t.Namespace == pageNamespace &&
+                t.GetCustomAttribute(typeof(RouteAttribute), false) is not null
+                );
+            // 2. Select the pages that use injected ScopedData
+            var pagesUsingScopedData = pages.Where(
+                page => page.GetRuntimeProperties().Any(property => 
+                    property.PropertyType == typeof(ScopedData) &&
+                    property.GetCustomAttribute(typeof(InjectAttribute), false) is not null)
+                );
+            // 3. Save the route attributes - effective URIs - of these pages
+            limitedPages = pagesUsingScopedData.Select(p => 
+            (p.GetCustomAttribute(typeof(RouteAttribute), false) as RouteAttribute).Template)
+                .Select(route => route.Remove(0, 1)) // remove the leading '/'
+                .Where(s => !string.IsNullOrEmpty(s))
+                .ToArray();
+
+            if (limitedPages.Length == 0)
+                throw new DataException($"Page reflection error - {nameof(limitedPages)} is empty");
+        }
+    }
+}
