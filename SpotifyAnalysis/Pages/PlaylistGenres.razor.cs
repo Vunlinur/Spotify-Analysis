fb644567a95e2ec049751d13d795b9daba744ac4
Vunlinur <mightymetalfan@gmail.com>
2025-10-05 16:52:52 +0200

    Use GenreRatio component

---
+++ SpotifyAnalysis/Pages/PlaylistGenres.razor.cs
@@ -0,0 +1,18 @@
+﻿using SpotifyAnalysis.Data.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace SpotifyAnalysis.Pages {
+	public partial class PlaylistGenres {
+		public class Playlist(PlaylistDTO playlistDTO, List<Genre> topGenres) {
+			public PlaylistDTO playlistDTO = playlistDTO;
+			public List<Genre> topGenres = topGenres;
+		}
+
+		public class Genre(string name, int count) {
+			public string name = name;
+			public int count = count;
+			public string color;
+		}
+	}
+}
