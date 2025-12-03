367ec57b87f140532f32d7f339386572a8cd7d63
Vunlinur <mightymetalfan@gmail.com>
2025-12-03 01:36:44 +0100

    Add Type and LastUpdated to AlbumDTO

--- SpotifyAnalysis/Data/DTO/AlbumDTO.cs
+++ SpotifyAnalysis/Data/DTO/AlbumDTO.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace SpotifyAnalysis.Data.DTO {
     [Table("Albums")]
@@ -11,6 +12,8 @@ namespace SpotifyAnalysis.Data.DTO {
 
         public string Name { get; set; }
 
+        public AlbumType Type { get; set; }
+
         public string ReleaseDate { get; set; }
 
         public int TotalTracks { get; set; }
@@ -19,12 +22,25 @@ namespace SpotifyAnalysis.Data.DTO {
 
         public string Label { get; set; }
 
-		public string ImageS { get; set; }
+        public string ImageS { get; set; }
 
-		public string ImageL { get; set; }
+        public string ImageL { get; set; }
 
-		public virtual List<ArtistDTO> Artists { get; set; }
+        public virtual List<ArtistDTO> Artists { get; set; }
 
         public virtual List<TrackDTO> Tracks { get; set; }
-	}
+
+        // Meta
+        public DateTime LastUpdated { get; set; }
+    }
+
+    // The relationship between the artist and the album
+    public enum AlbumType { album, single, compilation, appears_on }
+
+    public static class AlbumTypeExtensions {
+        public static AlbumType FromString(this AlbumType type, string from) {
+            Enum.TryParse(from, out AlbumType result);
+            return result;
+        }
+    }
 }
---
+++ SpotifyAnalysis/Migrations/20251203002502_AddAlbumTypeLastUpdated.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpotifyAnalysis.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAlbumTypeLastUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdated",
+                table: "Albums",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Albums",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdated",
+                table: "Albums");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Albums");
+        }
+    }
+}
