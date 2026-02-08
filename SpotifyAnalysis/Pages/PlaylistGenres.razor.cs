50142c57f8dd5fce6f9a220ccb4dd6ca03f370a8
Vunlinur <mightymetalfan@gmail.com>
2026-02-08 02:02:39 +0100

    Move genre coloring to GenreExtensions

--- SpotifyAnalysis/Pages/PlaylistGenres.razor.cs
+++ SpotifyAnalysis/Pages/PlaylistGenres.razor.cs
@@ -1,6 +1,9 @@
-﻿using SpotifyAnalysis.Data.DTO;
+﻿using Fernandezja.ColorHashSharp;
+using SpotifyAnalysis.Data.DTO;
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using static SpotifyAnalysis.Pages.PlaylistGenres;
 
 namespace SpotifyAnalysis.Pages {
 	public partial class PlaylistGenres {
@@ -13,6 +16,30 @@ namespace SpotifyAnalysis.Pages {
 			public string name = name;
 			public int count = count;
 			public string color;
+
+	public static class GenreExtensions {
+		public static readonly char[] splitOn = [' ', '-'];
+		private static readonly ColorHash colorHash = new();
+
+		public static Genre Color(this Genre genre) {
+			var tokens = genre.name
+				.ToLower()
+				.Replace("music", "")
+				.Split(splitOn, StringSplitOptions.RemoveEmptyEntries);
+			genre.root = tokens.Last(); // e.g. "rock" from "classic rock"
+			genre.color = '#' + colorHash.Hex(genre.root);
+			return genre;
+		}
+
+		public static void Color(this IEnumerable<Genre> genres, Dictionary<string, Genre> genreSamples = null) {
+			genreSamples ??= new(StringComparer.OrdinalIgnoreCase);
+
+			foreach (var genre in genres) {
+				if (!genreSamples.ContainsKey(genre.name))
+					genreSamples[genre.name] = genre.Color();
+				genre.root = genreSamples[genre.name].root;
+				genre.color = genreSamples[genre.name].color;
+			}
 		}
 	}
 }
