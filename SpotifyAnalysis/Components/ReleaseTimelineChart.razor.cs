7e5c014e819257d9b719b5f7a33fe103f31f01c6
Vunlinur <mightymetalfan@gmail.com>
2026-02-10 01:28:23 +0100

    Refactor GetChartOptions to not rebuild the ChartOptions each time to keep its ID

--- SpotifyAnalysis/Components/ReleaseTimelineChart.razor.cs
+++ SpotifyAnalysis/Components/ReleaseTimelineChart.razor.cs
@@ -12,80 +12,94 @@ namespace SpotifyAnalysis.Components {
 	}
 
 	public partial class ReleaseTimelineChart {
+		ApexChartOptions<AlbumDTO> ChartOptions => GetChartOptions(ArtistsWithReleases);
+		readonly ApexChartOptions<AlbumDTO> chartOptionsScoped = BuildRawChartOptions();
 
-		ApexChartOptions<AlbumDTO> chartOptions => BuildChartOptions(ArtistsWithReleases);
+		ApexChartOptions<AlbumDTO> GetChartOptions(IEnumerable<ArtistWithReleases> artistsWithReleases) {
+			int count = artistsWithReleases?.Count() ?? 0;
+			chartOptionsScoped.Chart.Height = CalculateHeight(artistsWithReleases);
+			chartOptionsScoped.Tooltip.Y.Formatter = CreateTooltipYFormatter(artistsWithReleases);
+			chartOptionsScoped.Yaxis[0].Max = count;
+			chartOptionsScoped.Yaxis[0].TickAmount = count + 1;
+			chartOptionsScoped.Yaxis[0].Labels.Formatter = CreateYAxisFormatter(artistsWithReleases);
+			return chartOptionsScoped;
+		}
 
-		private ApexChartOptions<AlbumDTO> BuildChartOptions(IEnumerable<ArtistWithReleases> artistsWithReleases) {
-			string[] alternatingLabelColors = ["#1aa14b", "#cccccc"];
+		/// Calculate the height of the chart based on the number of artists - more artists = less pixels per artist
+		private static double CalculateHeight(IEnumerable<ArtistWithReleases> artistsWithReleases) {
 			var count = artistsWithReleases?.Count() ?? 0;
+			// minimum 11px, ideally 27px per artist, +56 px for header & footer
+			// this reaches 11px at 115 artists: y=27-x/15*log(x+1)
+			var heightPerArtist = 27 - (count / 15f) * Math.Log10(count + 1);
+			return 56 + (count + 2) * Math.Max(heightPerArtist, 11);
+		}
 
+		/// Creates JS formatter replacing the numbers on the Y axis with artist names
+		private static string CreateYAxisFormatter(IEnumerable<ArtistWithReleases> artistsWithReleases) {
 			var artistNames = artistsWithReleases.Select(a => a.Artist?.Name ?? "Unknown").ToList();
 			var namesJson = JsonSerializer.Serialize(artistNames);
 			var yAxisFormatter = $"function(val) {{ var n = {namesJson}; return n[val] !== undefined ? n[val] : ''; }}";
+			return yAxisFormatter;
+		}
 
-			// display the release name in the tooltip for each bubble
+		/// Creates JS formatter to display the release name in the tooltip for each bubble
+		private static string CreateTooltipYFormatter(IEnumerable<ArtistWithReleases> artistsWithReleases) {
 			var releaseTitlesBySeries = artistsWithReleases?
 				.Select(a => a.RecentReleases.Select(r => r.Name).ToList())
 				.ToList() ?? [[]];
 			var releaseTitlesJson = JsonSerializer.Serialize(releaseTitlesBySeries);
 			var tooltipYFormatter = $"function(value, {{ seriesIndex, dataPointIndex }}) {{ var t = {releaseTitlesJson}; var s = t[seriesIndex]; return (s && s[dataPointIndex]) ? s[dataPointIndex] : value; }}";
+			return tooltipYFormatter;
+		}
 
-			// minimum 11px, ideally 27px per artist, +56 px for header & footer
-			// this reaches 11px at 115 artists: y=27-x/15*log(x+1)
-			var heightPerArtist = 27 - (count / 15f) * Math.Log10(count + 1);
-			var heightTotal = 56 + (count + 2) * Math.Max(heightPerArtist, 11);
-
-			var opts = new ApexChartOptions<AlbumDTO> {
-				Debug = true,
-				Theme = new Theme { Mode = Mode.Dark },
-				Chart = new Chart {
-					Height = heightTotal,
-					Background = "#ffffff00",
-					Zoom = new Zoom { AllowMouseWheelZoom = false }
+		private static ApexChartOptions<AlbumDTO> BuildRawChartOptions() => new() {
+			Debug = true,
+			Theme = new Theme { Mode = Mode.Dark },
+			Chart = new Chart {
+				Height = "",  // overwritten
+				Background = "#ffffff00",
+				Zoom = new Zoom { AllowMouseWheelZoom = false }
+			},
+			Grid = new Grid { BorderColor = "#1aa14b" },
+			PlotOptions = new PlotOptions {
+				Bubble = new PlotOptionsBubble { MinBubbleRadius = 4, MaxBubbleRadius = 16 }
+			},
+			Tooltip = new Tooltip {
+				Theme = Mode.Dark,
+				X = new TooltipX { Format = @"MMMM \ yyyy", Show = true },
+				Y = new TooltipY {
+					Title = new TooltipYTitle { Formatter = @"function(name) { return name + ':' }" },
+					Formatter = "",  // overwritten
 				},
-				Grid = new Grid { BorderColor = "#1aa14b" },
-				PlotOptions = new PlotOptions {
-					Bubble = new PlotOptionsBubble { MinBubbleRadius = 4, MaxBubbleRadius = 16 }
+				Z = new TooltipZ { Title = "Popularity:" }
+			},
+			Xaxis = new XAxis {
+				Title = new AxisTitle {
+					OffsetY = 5,
+					Text = "Month",
+					Style = new AxisTitleStyle { FontSize = "14px", Color = "#aaaaaa" }
 				},
-				Tooltip = new Tooltip {
-					Theme = Mode.Dark,
-					X = new TooltipX { Format = @"MMMM \ yyyy", Show = true },
-					Y = new TooltipY {
-						Title = new TooltipYTitle { Formatter = @"function(name) { return name + ':' }" },
-						Formatter = tooltipYFormatter
-					},
-					Z = new TooltipZ { Title = "Popularity:" }
-				},
-				Xaxis = new XAxis {
-					Title = new AxisTitle {
-						OffsetY = 5,
-						Text = "Month",
-						Style = new AxisTitleStyle { FontSize = "14px", Color = "#aaaaaa" }
-					},
-					Labels = new XAxisLabels { Style = new AxisLabelStyle { FontSize = "14px", Colors = "#aaaaaa" } },
-					AxisBorder = new AxisBorder { Height = 0 }
-				},
-				Legend = new Legend { Show = false },
-				Annotations = new Annotations { Yaxis = new List<AnnotationsYAxis>() },
-				Yaxis = new List<YAxis>() {
+				Labels = new XAxisLabels { Style = new AxisLabelStyle { FontSize = "14px", Colors = "#aaaaaa" } },
+				AxisBorder = new AxisBorder { Height = 0 }
+			},
+			Legend = new Legend { Show = false },
+			Annotations = new Annotations { Yaxis = new List<AnnotationsYAxis>() },
+			Yaxis = new List<YAxis>() {
 				new YAxis {
 					Min = -1,
-					Max = count,
-					TickAmount = count + 1,
+					Max = 10,  // overwritten
+					TickAmount = 11,  // overwritten
 					ForceNiceScale = false,
 					Title = new AxisTitle { Text = "Artist", Style = new AxisTitleStyle { FontSize = "14px", Color = "#aaaaaa" } },
 					Labels = new YAxisLabels {
 						Style = new AxisLabelStyle {
 							FontSize = "12px",
-							Colors = new Color(alternatingLabelColors)
+							Colors = new Color(["#1aa14b", "#cccccc"])
 						},
-						Formatter = yAxisFormatter
+						Formatter = "",  // overwritten
 					}
 				}
 			}
-			};
-
-			return opts;
-		}
+		};
 	}
 }
